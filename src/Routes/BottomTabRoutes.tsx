f554558fab068379a48acba170720a535ae14b65
MouraVocal <diego.moura3@gmail.com>
2021-11-23 21:31:26 -0300

    Add createMaterialBottomTabNavigator

--- src/Routes/BottomTabRoutes.tsx
+++ src/Routes/BottomTabRoutes.tsx
@@ -1,34 +1,45 @@
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import GraphicsScreen from '../screens/Graphics';
 import { Home } from '../screens/Home';
 import { SalesScreen } from '../screens/Sales';
+import { AntDesign } from '@expo/vector-icons';
+import { COLORS } from '../theme';
 
 export function BottomTabRoutes() {
 
-	const Tab = createBottomTabNavigator();
+	const Tab = createMaterialBottomTabNavigator();
   
 	return (
 		<NavigationContainer>
 			<Tab.Navigator
-				screenOptions={{
-					headerShown: false
-				}}
-				initialRouteName='Início'
+				initialRouteName="Home"
+				activeColor={COLORS.WHITE}
+				inactiveColor={COLORS.BLUE}
+				barStyle={{ backgroundColor: COLORS.DARK_BLUE }}
 			>
 				<Tab.Screen
 					name="Histórico"
 					component={GraphicsScreen}
+					options={{
+						tabBarIcon: ({ color })=> (<AntDesign name="linechart" size={23} color={color} />),
+					}}
 				/>
 				<Tab.Screen
 					name="Início"
 					component={Home}
+					options={{
+						tabBarIcon: ({ color })=> <AntDesign name="home" size={23} color={color} />,
+					}}
 				/>
 				<Tab.Screen
 					name="Ofertas"
 					component={SalesScreen}
+					options={{
+						tabBarIcon: ({ color })=> <AntDesign name="wallet" size={23} color={color} />,
+					}}
+					
 				/>
 			</Tab.Navigator>
 		</NavigationContainer>
